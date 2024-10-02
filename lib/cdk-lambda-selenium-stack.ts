6dacbe4e163e3607902c45dfef556691a106bfc7
TAPIOKA1113 <manato20021113@gmail.com>
2024-10-03 08:00:17 +0900

    main

--- lib/cdk-lambda-selenium-stack.ts
+++ lib/cdk-lambda-selenium-stack.ts
@@ -1,16 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class CdkLambdaSeleniumStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaSeleniumQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dockerImage = new cdk.aws_ecr_assets.DockerImageAsset(
+      this,
+      "SeleniumDockerImage",
+      {
+        directory: path.join(__dirname, "../", "app"),
+      }
+    );
+    new cdk.aws_lambda.DockerImageFunction(this, "SeleniumLambda", {
+      functionName: "selenium-lambda",
+      code: cdk.aws_lambda.DockerImageCode.fromEcr(dockerImage.repository, {
+        tagOrDigest: dockerImage.imageTag,
+      }),
+      memorySize: 2048,
+      timeout: cdk.Duration.seconds(90),
+    });
   }
 }
